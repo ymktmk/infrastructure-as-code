d9e9a5b4833a4456023de92767587644806f16ba
ymktmk <73768462+ymktmk@users.noreply.github.com>
2022-07-29 21:40:12 +0900

    fix

--- terraform/eks-on-fargate/eks.tf
+++ terraform/eks-on-fargate/eks.tf
@@ -14,9 +14,9 @@ module "eks" {
 
   eks_managed_node_groups = {
     green = {
-      min_size       = 2
-      max_size       = 4
-      desired_size   = 3
+      min_size       = 1
+      max_size       = 1
+      desired_size   = 1
       instance_types = ["t3.large"]
     }
   }
--- terraform/eks-on-fargate/secrets-manager.tf
+++
@@ -1,9 +0,0 @@
-# resource "aws_secretsmanager_secret" "password" {
-#   name                           = "aws/password"
-#   force_overwrite_replica_secret = false
-#   lifecycle {
-#     ignore_changes = [
-#       replica,
-#     ]
-#   }
-# }
