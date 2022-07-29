4c2b330f49e2503e806e710a44ced37a49328548
ymktmk <73768462+ymktmk@users.noreply.github.com>
2022-07-29 15:57:39 +0900

    fix

--- terraform/aws-parts/oidc.tf
+++ terraform/aws-parts/oidc.tf
@@ -3,7 +3,7 @@ resource "aws_iam_openid_connect_provider" "github_actions" {
 
   client_id_list = ["sts.amazonaws.com"]
 
-  thumbprint_list = ["a031c46782e6e6c662c2c87c76da9aa62ccabd8e"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
 }
 
 resource "aws_iam_role" "github_actions_oidc_test" {
@@ -21,7 +21,10 @@ resource "aws_iam_role" "github_actions_oidc_test" {
         "Action" : "sts:AssumeRoleWithWebIdentity",
         "Condition" : {
           "StringEquals" : {
-            "token.actions.githubusercontent.com:sub" : "repo:ymktmk/infrastructure-as-code:ref:refs/heads/main"
+            "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com"
+          },
+          "StringLike" : {
+            "token.actions.githubusercontent.com:sub" : "repo:ymktmk/infrastructure-as-code:*"
           },
         }
       }
