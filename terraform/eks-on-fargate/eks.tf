e71f1a6b3ea7226911b625cb26709baacc04baf5
ymktmk <ymktmk.tt@gmail.com>
2022-08-01 19:47:24 +0900

    add external-app

--- terraform/eks-on-fargate/eks.tf
+++ terraform/eks-on-fargate/eks.tf
@@ -16,8 +16,8 @@ module "eks" {
   eks_managed_node_groups = {
     green = {
       min_size       = 1
-      max_size       = 1
-      desired_size   = 1
+      max_size       = 3
+      desired_size   = 2
       instance_types = ["t3.large"]
     }
   }
